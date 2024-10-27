a09988ab90e88014327ccf15be264b569be577e5
ваше имя или ник латиницей <dmitry.bazyk@yandex.ru>
2024-10-28 02:54:17 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import myImage from './images/modern.jpg';
-
-console.log(myImage);
-
-console.log('Hello World!, Hello JavaScript!');
+import commonjs from '@rollup/plugin-commonjs';
 
 const imgElement = document.createElement('img');
 imgElement.src = myImage; // Используем импортированное изображение
+imgElement.alt = 'Modern Image'; // Добавьте атрибут alt для лучшей доступности
 document.body.appendChild(imgElement);
+
+console.log(myImage);
+console.log('Hello World!, Hello JavaScript!');
+
+export default {
+    plugins: [commonjs()]
+};
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,47 +1,19 @@
-import postcss from 'rollup-plugin-postcss';
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+import image from '@rollup/plugin-image';
 
-module.exports = {
-  mode: 'development',
-  entry: path.resolve(__dirname, 'src', 'index.js'),
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true, /* для очистки 'dist' после сборки проекта */
-  },
-  devServer: {
-    static: path.resolve(__dirname, 'dist'),
-    port: 8080,
-    open: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-    template: path.resolve(__dirname, 'index.html'),
-  })
-],
-module: {
-    rules: [
-      {
-        test: /\.(?:js|mjs|cjs)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            targets: "defaults",
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+
+
+export default {
+    input: 'index.js',
+    output: {
+        file: 'bundle.js',
+        format: 'iife', // или 'umd', в зависимости от ваших нужд
+        name: 'MyBundle'
+    },
+    plugins: [
+        resolve(), // Позволяет Rollup находить модули в node_modules
+        commonjs(), // Конвертирует CommonJS в ES модули
+        image() // Позволяет импортировать изображения
     ]
-  }
 };
-
