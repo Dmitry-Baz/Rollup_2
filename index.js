5b29e96412eb431fdecf820d2b7276591236b7e4
ваше имя или ник латиницей <dmitry.bazyk@yandex.ru>
2024-10-27 19:21:58 +0700

    Initial commit_2

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+import myImage from './images/modern.jpg';
+
+console.log(myImage);
 
 console.log('Hello World!, Hello JavaScript!');
+
+const imgElement = document.createElement('img');
+imgElement.src = myImage; // Используем импортированное изображение
+document.body.appendChild(imgElement);
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,9 +1,47 @@
-export default {
-    input: './index.js',
-            
-    output: {
-      dir: './build/bundle.js',
-      format: 'cjs',
-      inlineDynamicImports: 'true'
-    }
-  };
+import postcss from 'rollup-plugin-postcss';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, /* для очистки 'dist' после сборки проекта */
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, 'index.html'),
+  })
+],
+module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            targets: "defaults",
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  }
+};
+
